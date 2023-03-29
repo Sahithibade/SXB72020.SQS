869b217c0ac0b20e679dea9f9435118b14065235
sahithibade <SXB72020@UCMO.EDU>
2023-03-28 20:08:37 -0500

    version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import json
 sqs=boto3.resource('sqs')
 queue=sqs.create_queue(
- QueueName='sqs_module'
+ QueueName='SQSASSIGNMENT3'
  
  )
-print("Created queue '%s' with URL=%s",'sqs_module',queue.url)
+print("Created queue '%s' with URL=%s",'myPytonQueue',queue.url)
